59362f0060d272381e0ea0fe80fa3ac85f837d8b
tiwarivivek44 <tiwarivivek44@gmail.com>
2022-08-05 10:27:04 +0530

    Added new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -164,6 +164,10 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+const newFeature = function () {
+  console.log('Welcome to the application!');
+};
+
 //////////////////////////////////////////////////////////////////////////////////
 // Init function
 //////////////////////////////////////////////////////////////////////////////////
@@ -175,6 +179,7 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
+  newFeature();
 };
 
 init();
